3b7ac99aab7d9a24ce1e2e6f8fd164b5740c595a
Gabriel <gabrielclimb@gmail.com>
2020-02-23 10:30:32 -0300

    fix test

--- Mann_Kendall_Automated/tests/context.py
+++ Mann_Kendall_Automated/tests/context.py
@@ -1,3 +1,4 @@
-from utils.fix_string import string_test, string_to_float
-from utils.mann_kendall import mk_test
+from mann_kendall_automated.utils.fix_string import string_test,\
+    string_to_float
+from mann_kendall_automated.utils.mann_kendall import mk_test
 
