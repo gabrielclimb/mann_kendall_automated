44f8cbc874dfb7e2ecfde27a91b090c803e40452
Gabriel Barbosa Soares <gabriel.soares@saltpay.co>
2023-06-06 21:21:36 +0100

    remove progress bar test

--- src/tests/context.py
+++ src/tests/context.py
@@ -1,15 +1,14 @@
 import os
 import sys
 
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-)  # noqa
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # pylint: disable=unused-import
-from src.generate import generate_mann_kendall, transpose_dataframe  # noqa
-from src.utils.fix_string import (  # noqa
+from src.generate import generate_mann_kendall, transpose_dataframe
+from src.utils.fix_string import (
     get_columns_with_incorrect_values,
     string_test,
     string_to_float,
 )
-from src.utils.mann_kendall import mk_test  # noqa
+from src.utils.mann_kendall import mk_test
+from src.utils.progress_bar import print_progress_bar
